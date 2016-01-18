fcf341a713861ca236335927935a2344e655c9dc
ThomOverhand <thom_overhand@hotmail.com>
2016-01-18 19:30:31 +0100

    Mouse click
    Mouse click added to hover script
    Scene must be implemented

--- Assets/Scripts/HoverScript.cs
+++ Assets/Scripts/HoverScript.cs
@@ -25,6 +25,21 @@ public class HoverScript : MonoBehaviour {
         tm.color = Color.white;
     }
 
+    public bool isStart;
+    public bool isQuit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            Application.LoadLevel();
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+
     // Update is called once per frame
     void Update () {
 	
