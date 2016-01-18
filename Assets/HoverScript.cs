7a60f4160003962504ba5a53660d4684e4d93b97
ThomOverhand <thom_overhand@hotmail.com>
2016-01-18 15:57:20 +0100

    Main menu
    Basic main menu added

---
+++ Assets/HoverScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        //renderer.material.color = Color.black;
+        TextMesh tm = gameObject.GetComponent<TextMesh>();
+        tm.color = Color.white;
+    }
+
+    void OnMouseEnter()
+    {
+        //renderer.material.color = Color.red;
+        TextMesh tm = gameObject.GetComponent<TextMesh>();
+        tm.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        //renderer.material.color = Color.black;
+        TextMesh tm = gameObject.GetComponent<TextMesh>();
+        tm.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
