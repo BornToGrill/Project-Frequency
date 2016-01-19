b492d128d6f3f0b5b9459bab38ff26916399b3e7
bart-rijnders <mail@bartrijnders.com>
2016-01-19 15:56:50 +0100

    Pathfinding script adjusted

--- Assets/Scripts/AStarPathfinding.cs
+++ Assets/Scripts/AStarPathfinding.cs
@@ -6,77 +6,64 @@ public static class AStarPathfinding {
 
 	private static List<Node> OpenList = new List<Node> ();
 	private static List<Node> ClosedList = new List<Node> ();
-	private static Node _endLocation;
-	private static Node _startLocation;
 
-	public static Node EndLocation {
-		get { return _endLocation;}
-		set { _endLocation = value;
-			FindPath(StartLocation, _endLocation);
+	public static List<Node> FindPath(Node startNode, Node endNode) {
+		if (startNode == null || endNode == null) {
+			return null;
 		}
+		if (startNode == endNode)
+			return null;
+		
+		PathFinder(startNode, endNode);
+		return ConvertNodesToList (startNode, endNode);
 	}
-	public static Node StartLocation {
-		get { return _startLocation; }
-		set { _startLocation = value;
-			FindPath(_startLocation, EndLocation);
+
+	static List<Node> ConvertNodesToList(Node startNode, Node endNode) {
+		Node currentNode = endNode;
+		List<Node> nodeList = new List<Node> ();
+		while (currentNode != startNode) {
+			nodeList.Add (currentNode);
+			currentNode = currentNode.Parent;
 		}
+		nodeList.Add (startNode);
+		return nodeList;
 	}
 
-	public static void FindPath(Node start, Node end) {
-		if (start == null || end == null) {
-			Debug.Log ("one is null");
+	static void PathFinder(Node currentNode, Node endNode) {
+		if (currentNode == endNode)
 			return;
-		}
-		if (start == end)
-			return;
-		PathFinder(start);
-		//PrintPath (EndLocation);
-	    int pathLength = GetPathLength(EndLocation);
-        Debug.Log("Distance to node : " + pathLength.ToString());
-	}
-
-	static int GetPathLength(Node node) {
-		//Debug.Log (node.X.ToString() + " : " + node.Y.ToString());
-		if (node.Parent == null)
-			return 0;
-	    return 1 + GetPathLength(node.Parent);
-	    //PrintPath (node.Parent);
-	}
-
-	static void PathFinder(Node node) {
-		if (EndLocation.Parent != null)
-			return;
-		OpenList.Remove (node);
-		ClosedList.Add (node);
-		UpdateSurroundingNodes (node);
+		OpenList.Remove (currentNode);
+		ClosedList.Add (currentNode);
+		UpdateSurroundingNodes (currentNode, endNode);
 		if (OpenList.Count < 1)
 			return;
 		
-		Node bestF = OpenList [0];
-		foreach (Node possibleNextNode in OpenList)
-			if (bestF.FCost > possibleNextNode.FCost)
-				bestF = possibleNextNode;
-			else if (bestF.FCost == possibleNextNode.FCost && bestF.HCost > possibleNextNode.HCost)
-				bestF = possibleNextNode;
-		PathFinder (bestF);
+		Node cheapestNode = OpenList [0];
+		foreach (Node nextNode in OpenList)
+			if (cheapestNode.FCost >nextNode.FCost)
+				cheapestNode = nextNode;
+			else if (cheapestNode.FCost == nextNode.FCost && cheapestNode.HCost > nextNode.HCost)
+				cheapestNode =nextNode;
+		
+		PathFinder (cheapestNode, endNode);
 	}
 
-	static void UpdateSurroundingNodes(Node node){
+	static void UpdateSurroundingNodes(Node node, Node endNode){
 		if (node.Left != null && !ClosedList.Contains (node.Left)) {
 			OpenList.Add (node.Left);
-			node.Left.UpdateNode (node, EndLocation);
+			node.Left.UpdateNode (node, endNode);
 		}
 		if (node.Right != null && !ClosedList.Contains (node.Right)) {
 			OpenList.Add (node.Right);
-			node.Right.UpdateNode (node, EndLocation);
+			node.Right.UpdateNode (node, endNode);
 		}
 		if (node.Up != null && !ClosedList.Contains (node.Up)) {
 			OpenList.Add (node.Up);
-			node.Up.UpdateNode (node, EndLocation);
+			node.Up.UpdateNode (node, endNode);
 		}
 		if (node.Down != null && !ClosedList.Contains (node.Down)) {
 			OpenList.Add (node.Down);
-			node.Down.UpdateNode (node, EndLocation);
+			node.Down.UpdateNode (node, endNode);
 		}
 	}
 }
