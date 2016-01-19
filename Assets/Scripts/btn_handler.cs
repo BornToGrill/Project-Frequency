0acc8f6e004523b8dd61f14fc3080988467890d2
Stephen Goedhart <sdg25@hotmail.com>
2016-01-19 09:14:05 +0100

    Instructions commit by Stephen
    Added a navigation scene and menu to the game that links back to the
    Main Menu.

---
+++ Assets/Scripts/btn_handler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btn_handler : MonoBehaviour
+{
+
+    public void LoadScene(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
