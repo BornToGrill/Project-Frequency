afe039221d44a20b1b889af6f391889b3d8948c0
bart-rijnders <mail@bartrijnders.com>
2016-01-20 22:10:45 +0100

    Started refactoring pathfinding

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,7 +8,8 @@ public class Board : MonoBehaviour {
 	public int BoardDimensions;
 	public GameObject[,] _tiles;
 
-	void Awake () {
+	void Awake ()
+	{
 		_tiles = new GameObject[BoardDimensions, BoardDimensions];
 		for (int x = 0; x < _tiles.GetLength (0); x++)
 			for (int y = 0; y < _tiles.GetLength (1); y++)
@@ -16,30 +17,61 @@ public class Board : MonoBehaviour {
 		GetSurroundingTiles();
 	}
 
-	void GetSurroundingTiles() {
-		for (int x = 0; x < _tiles.GetLength (0); x++)
-			for (int y = 0; y < _tiles.GetLength (1); y++) {
-				Node tile = _tiles [x, y].GetComponent<Node> ();
-				if (x > 0)
-					tile.Left = _tiles [x -1 , y].GetComponent<Node> ();
-				if (x < BoardDimensions-1)
-					tile.Right = _tiles [x + 1, y].GetComponent<Node> ();
-				if (y > 0)
-					tile.Up = _tiles [x, y - 1].GetComponent<Node> ();
-				if (y < BoardDimensions-1)
-					tile.Down = _tiles [x, y + 1].GetComponent<Node> ();
+
+	private bool triggercheck = true;
+
+	void Update() {
+		if (triggercheck && Input.GetKeyDown("space")) {
+			triggercheck = false;
+			List<TileController> x = Pathfinding.FindPath (_tiles [0, 0].GetComponent<TileController>(), _tiles [5, 5].GetComponent<TileController>());
+			Debug.Log (x.Count);
+			foreach (TileController tile in x) {
+				tile.Environment = Environment.None;
 			}
+		}
 	}
 
-	Environment GetEnvironment(int posX, int posY) {
-		if (posY < Math.Floor(BoardDimensions * 0.4f)) {
+	GameObject CreateTile(int posX, int posY)
+	{
+		GameObject go = Instantiate (TilePrefab, new Vector3(posX, posY, 0), new Quaternion()) as GameObject;
+		go.transform.SetParent (transform);
+		TileController tile = go.GetComponent<TileController> ();
+		tile.Environment = GetEnvironment (posX, posY);
+		tile.Position = new Vector2 (posX, posY);
+		return go;
+	}
+
+	void GetSurroundingTiles()
+	{
+		for (int x = 0; x < _tiles.GetLength (0); x++)
+		{
+			for (int y = 0; y < _tiles.GetLength (1); y++)
+			{
+				TileController tile = _tiles [x, y].GetComponent<TileController> ();
+				if (x > 0)
+					tile.Left = _tiles [x - 1, y].GetComponent<TileController> ();
+				if (x < BoardDimensions - 1)
+					tile.Right = _tiles [x + 1, y].GetComponent<TileController> ();
+				if (y > 0)
+					tile.Up = _tiles [x, y - 1].GetComponent<TileController> ();
+				if (y < BoardDimensions - 1)
+					tile.Down = _tiles [x, y + 1].GetComponent<TileController> ();
+			}
+		}
+	}
+
+	Environment GetEnvironment(int posX, int posY)
+	{
+		if (posY < Math.Floor(BoardDimensions * 0.4f))
+		{
 			if (posX < Math.Floor(BoardDimensions * 0.4f))
 				return Environment.Desert;
 			if (posX > BoardDimensions * 0.6f)
 				return Environment.Forest;
 			return Environment.Water;
 		}
-		if (posY > BoardDimensions * 0.6f) {
+		if (posY > BoardDimensions * 0.6f)
+		{
 			if (posX < Math.Floor(BoardDimensions * 0.4f))
 				return Environment.Swamp;
 			if (posX > BoardDimensions * 0.6f)
@@ -49,16 +81,5 @@ public class Board : MonoBehaviour {
 		if (posX < Math.Floor(BoardDimensions * 0.4f) || posX > BoardDimensions * 0.6f)
 			return Environment.Water;
 		return Environment.Island;
-	}
-
-	GameObject CreateTile(int posX, int posY) {
-		GameObject go = Instantiate (TilePrefab, new Vector3(posX, posY, 0), new Quaternion()) as GameObject;
-		go.transform.parent = transform;
-		TileController tile = go.GetComponent<TileController> ();
-		tile.Environment = GetEnvironment (posX, posY);
-		Node node = go.GetComponent<Node> ();
-		node.X = posX;
-		node.Y = posY;
-		return go;
 	}
 }
