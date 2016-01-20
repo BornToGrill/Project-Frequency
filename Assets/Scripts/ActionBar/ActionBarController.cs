e6016c68728bb4356ba19f261e60e76e2bd36bd2
Stephen Goedhart <sdg25@hotmail.com>
2016-01-20 16:33:24 +0100

    refactored actionbar code met Bart

--- Assets/Scripts/ActionBar/ActionBarController.cs
+++ Assets/Scripts/ActionBar/ActionBarController.cs
@@ -7,44 +7,32 @@ public delegate void Callback();
 public class ActionBarController : MonoBehaviour {
     public float margin;
     public GameObject ActionButtonPrefab;
-    private List<GameObject> _actionButtons;
-    // Use this for initialization
-	void Start () {
-        _actionButtons = new List<GameObject>();
-        AddButton("hagrid", () => Debug.Log("clicked"));
-        AddButton("Gogogo", Clear);
-        AddButton("Hello", Bart);
-        
-	}
-	void Bart()
+    private List<GameObject> _buttons;
+
+    void Start ()
     {
-        AddButton("Gogogo", Clear);
+        _buttons = new List<GameObject>();
     }
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    	
     public void AddButton(string text, Callback callback) {
         float offset = 0;
-        foreach ( GameObject currentButton in _actionButtons)
+        foreach ( GameObject currentButton in _buttons)
         {
             RectTransform rectTransform = (RectTransform)currentButton.transform;
             offset += rectTransform.rect.width;
             offset += margin;
         }
-        
         GameObject button = Instantiate(ActionButtonPrefab) as GameObject;
         button.transform.SetParent(transform);
         button.GetComponent<ActionButtonController>().Initialize(text, callback, offset);
-        _actionButtons.Add(button);
-        Debug.Log(_actionButtons.Count);
+        _buttons.Add(button);
+        Debug.Log(_buttons.Count);
     }
+
     public void Clear()
     {
-        foreach(GameObject go in _actionButtons)
-        {
+        foreach(GameObject go in _buttons)
             GameObject.Destroy(go);
-        }
-        _actionButtons.Clear();
+        _buttons.Clear();
     }
 }
--- Assets/Scripts/ActionBar/ActionButtonController.cs
+++ Assets/Scripts/ActionBar/ActionButtonController.cs
@@ -4,42 +4,27 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ActionButtonController : MonoBehaviour, IPointerClickHandler {
-    private Text buttonText;
-    
-    private Callback callback; 
+    private Text _buttonText;
+    private Callback _callback;
 
-	// Use this for initialization
-	void Awake () {
-        buttonText = gameObject.transform.Find("Text").gameObject.GetComponent<Text>();
-        //Initialize("Hagrid", ()=> Debug.Log("gogogo"));
-	}
+    void Awake()
+    {
+        _buttonText = gameObject.transform.Find("Text").gameObject.GetComponent<Text>();
+    }
 	
 	public void OnPointerClick(PointerEventData data)
     {
-        if (callback != null)
-          callback.Invoke();
-        
+        if (_callback != null)
+            _callback.Invoke();
     }
 
     public void Initialize(string text, Callback callback, float offset)
     {
-        this.callback = callback;
-        buttonText.text = text;
+        this._callback = callback;
+        _buttonText.text = text;
         RectTransform rectTransform = transform as RectTransform;
-        //transform.position = new Vector3(0, 0, 0);
-        //transform.localScale = new Vector3(1, 1, 0);
         rectTransform.pivot = new Vector2(0.5f, 0.5f); 
         rectTransform.localScale = new Vector3(1, 1, 0);
-        //rectTransform.position = new Vector3(offset, 0, 0);
-        //rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchoredPosition = new Vector2(offset+50, 50);
-
-
-
-
-
-        
-        
-
     }
 }
