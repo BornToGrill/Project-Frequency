b77831d0a494ce467c466f84a6086634ed1b8f3e
Daniel <danielmolenaar@hotmail.nl>
2016-01-23 12:48:15 +0100

    All structures now use one controller.
    Moved all structure code back to the StructureUnit class. No BaseUnit
    class or BarrackUnit class necessary.

--- Assets/Scripts/Units/StructureUnit.cs
+++ Assets/Scripts/Units/StructureUnit.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System;
+using System.Linq;
 
 public class StructureUnit : BaseUnit {
 
-	private int _stackSize;
+    public GameObject[] BuildableUnits;
 
-	internal override int StackSize {
-		get { return _stackSize; }
+
+    private bool _isBuilding;
+    private GameObject _buildType;
+
+    internal override int StackSize {
+		get { return 1; }
 		set {
 		    if (value > MaxUnitStack)
 		        throw new ArgumentOutOfRangeException(string.Format("Structure can only have {0} stacked units", MaxUnitStack));
-		    _stackSize = value;
 		}
 	}
 
@@ -39,11 +43,33 @@ public class StructureUnit : BaseUnit {
 	}
 
     public override DeselectStatus OnFirstSelected(GameObject firstTile) {
-        return DeselectStatus.Both;
+
+        ActionBarController actionBar = GameObject.Find("ActionBar").GetComponent<ActionBarController>();
+        foreach (GameObject unit in BuildableUnits) {
+            actionBar.AddButton(unit.name, CreateUnit); // TODO: TEMP
+        }
+
+        return DeselectStatus.None;
     }
 
     public override DeselectStatus OnSecondClicked(GameObject firstTile, GameObject secondTile) {
+        if (!_isBuilding)
+            return DeselectStatus.Both;
+        //if secondTile is within building range
+        //if secondTile has no unit
+        GameObject unit = (GameObject)Instantiate(_buildType, secondTile.transform.position, new Quaternion());
+        BaseUnit unitBase = unit.GetComponent<BaseUnit>();
+        unitBase.Owner = Owner;
+        secondTile.GetComponent<TileController>().Unit = unitBase;
+
+        _isBuilding = false;
+        _buildType = null;
         return DeselectStatus.Both;
+    }
+
+    public void CreateUnit(string unitName) {
+        _isBuilding = true;
+        _buildType = BuildableUnits.Single(x => x.name == unitName);
     }
 
     public override void OnMouseEnter(GameObject firstTile, GameObject secondTile) {
