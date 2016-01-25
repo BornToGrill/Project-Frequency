745712d104f010da960da91cdae9dd24441bad45
Daniel <danielmolenaar@hotmail.nl>
2016-01-25 19:57:51 +0100

    All boat movement works now

--- Assets/Scripts/Units/UnitEventControllers/WaterUnitEventController.cs
+++ Assets/Scripts/Units/UnitEventControllers/WaterUnitEventController.cs
@@ -7,13 +7,37 @@ using UnityEngine;
 public class WaterUnitEventController : LandUnitEventController {
 
 
-
     #region Movement
     public override DeselectStatus MoveToEmpty(TileController start, List<TileController> path) {
 
+        MoveWaterUnit(start, path, null);
 
+        return DeselectStatus.Both;
+    }
+    public override DeselectStatus MoveToAttack(TileController start, List<TileController> path) {
+        LandUnit carry = ((WaterUnit) start.Unit).CarryUnit.GetComponent<LandUnit>();
+        List<TileController> movePath = path.Take(path.Count - carry.Range).ToList();
+        MoveWaterUnit(start, movePath,
+            () => {
+                carry.Attack(path.Last().Unit);
+            });
+
+        return DeselectStatus.Both;
+    }
+
+    public override DeselectStatus MoveToMerge(TileController start, List<TileController> path) {
+        LandUnit carry = ((WaterUnit) start.Unit).CarryUnit.GetComponent<LandUnit>();
+        LandUnit mergeTarget = (LandUnit) path.Last().Unit;
+        MoveWaterUnit(start, path, () => { mergeTarget.Merge(carry); });
+
+        path.Last().Unit = mergeTarget;
+
+        return DeselectStatus.Both;
+    }
+
+    private void MoveWaterUnit(TileController startTile, List<TileController> path, Action finalAction) {
         GameObject carryUnit = GetComponent<WaterUnit>().CarryUnit;
-        WaterUnit startBoat = (WaterUnit)start.Unit;
+        WaterUnit startBoat = (WaterUnit) startTile.Unit;
         BaseUnit unit = null;
         int index = -1;
 
@@ -30,43 +54,16 @@ public class WaterUnitEventController : LandUnitEventController {
             TileController[] boatPath = path.Take(index).ToArray();
             TileController[] unitPath = path.Skip(index).ToArray();
 
-            var x = ((WaterUnit) start.Unit);
-            var y = unit.GetComponent<LandUnitEventController>();
             StartCoroutine(AnimateToTile(boatPath, () => {
                 startBoat.UnloadUnit(boatPath.Last().gameObject);
-                unit.StartCoroutine(unit.GetComponent<LandUnitEventController>().AnimateToTile(unitPath));
+                unit.StartCoroutine(unit.GetComponent<LandUnitEventController>().AnimateToTile(unitPath, finalAction));
             }));
             boatPath.Last().Unit = GetComponent<BaseUnit>();
             unitPath.Last().Unit = unit;
         }
-        start.Unit = null;
-
-
-        return DeselectStatus.Both;
-    }
-    public override DeselectStatus MoveToAttack(TileController start, List<TileController> path) {
-        List<TileController> movePath = path.Take(path.Count - GetComponent<LandUnit>().Range).ToList();
-        if (movePath.Count > 0) {
-            start.Unit = null;
-            movePath.Last().Unit = GetComponent<BaseUnit>();
-        }
-
-        StartCoroutine(AnimateToTile(movePath, () => {
-            GetComponent<LandUnit>().Attack(path.Last().Unit);
-        }));
-
-        return DeselectStatus.Both;
+        startTile.Unit = null;
     }
 
-    public override DeselectStatus MoveToMerge(TileController start, List<TileController> path) {
-        start.Unit = null;
-
-        StartCoroutine(AnimateToTile(path, () => {
-            ((LandUnit) path.Last().Unit).Merge(GetComponent<BaseUnit>());
-        }));
-
-        return DeselectStatus.Both;
-    }
     #endregion
 
 }
