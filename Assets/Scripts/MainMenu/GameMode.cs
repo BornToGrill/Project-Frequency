ea6d3a7ed432add0f249c858e64e8ea18a8e3ceb
ThomOverhand <jmaasy@gmail.com>
2016-01-28 17:27:40 +0100

    network option added

---
+++ Assets/Scripts/MainMenu/GameMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameMode : MonoBehaviour {
+	public void startGame() {
+		SceneManager.LoadScene ("MainGame");
+	}
+
+	public void startMultiplayer() {
+		SceneManager.LoadScene ("Lobby");
+	}
+}
