c8fcf379b687d0fa394b729074f6d5ca377fb032
Daniel <danielmolenaar@hotmail.nl>
2016-01-28 21:36:55 +0100

    Changed game selection screen

---
+++ Assets/Scripts/MainMenu/LocalPlay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LocalPlay : MonoBehaviour {
+
+
+    public void IncrementPlayers(int incr) {
+        GameObject go = GameObject.Find("PlayerAmount");
+        int current = int.Parse(go.GetComponent<Text>().text);
+        current += incr;
+        current = current > 4 ? 4 : current;
+        current = current < 2 ? 2 : current;
+        go.GetComponent<Text>().text = current.ToString();
+    }
+
+    public void StartLocal() {
+        GameObject localSettings = new GameObject("LocalGameSettings");
+        GameData data = localSettings.AddComponent<GameData>();
+        data.AmountOfPlayers = int.Parse(GameObject.Find("PlayerAmount").GetComponent<Text>().text);
+        DontDestroyOnLoad(localSettings);
+        SceneManager.LoadScene("MainGame");
+    }
+}
+
+
+public class GameData : MonoBehaviour {
+    public int AmountOfPlayers;
+}
--- Assets/Scripts/MainMenu/OnlinePlay.cs
+++ Assets/Scripts/MainMenu/OnlinePlay.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test : MonoBehaviour {
+public class OnlinePlay : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
