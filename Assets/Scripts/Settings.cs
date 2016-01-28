3b9a3c499f563b0442471d35c5e950481de62837
ThomOverhand <jmaasy@gmail.com>
2016-01-28 10:31:40 +0100

    Finished settings menu
    All components are working.

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -8,17 +8,26 @@ public class Settings : MonoBehaviour {
     Dropdown dropdown;
 	Toggle toggle;
 	Toggle mute;
+	Toggle grid;
 
-    Resolution[] PossibleResolutions;
+	Resolution[] PossibleResolutions;
 
 	public bool isMute;
 	public bool isWindowed;
 	public bool setResolution;
 	public bool isGrid;
+	public bool activated;
+	static public Color color;
+
+	public bool Mute;
+	public bool Windowed;
+	public bool Grid;
+	public string Resolution;
 
     // Use this for initialization
     void Start()
     {
+		DontDestroyOnLoad(gameObject.GetComponent<Settings>());
 		if (setResolution) {
 			dropdown = GetComponent<Dropdown> ();
 			PossibleResolutions = Screen.resolutions;
@@ -47,8 +56,19 @@ public class Settings : MonoBehaviour {
 			mute.onValueChanged.AddListener ((x) => {
 				muteSound (x);
 			});
+
+			if (AudioListener.volume == 0)
+				mute.isOn = true;
+			else
+				mute.isOn = false;
+			
 		} else if (isGrid) {
-			// TODO: Make grid switch on and off.
+			grid = GetComponent<Toggle> ();
+			grid.onValueChanged.AddListener ((x) => {
+				toggleGrid (x);
+			});
+
+			grid.isOn = activated;
 		} else {
 			return;
 		}
@@ -66,9 +86,18 @@ public class Settings : MonoBehaviour {
 			AudioListener.volume = 0;
 	}
 
-	public void OnDropDownChanged(int index)
-	{
+	public void OnDropDownChanged(int index){
 		Screen.SetResolution(PossibleResolutions[index].width, PossibleResolutions[index].height, Screen.fullScreen);
+	}
+
+	public void toggleGrid(bool toggle) {
+		if (toggle) {
+			color = new Color (1, 1, 1, 0);
+			activated = true;
+		} else {
+			color = new Color (1, 1, 1, .3f);
+			activated = false;
+		}
 	}
 }
 
