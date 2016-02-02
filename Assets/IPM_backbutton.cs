66b568d1fa35730f3c62dd9af765841c13914de9
StephenGoedhart <sdg25@hotmail.com>
2016-02-02 16:59:40 +0100

    Pause Menu Instructions

---
+++ Assets/IPM_backbutton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IPM_backbutton : MonoBehaviour {
+
+    public GameObject Instructions;
+
+	public void ADifferentName()
+    {
+        Instructions.SetActive(false);
+    }
+}
---
+++ Assets/SetActiveHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetActiveHandler : MonoBehaviour {
+
+    public GameObject Object;
+   
+	public void SetObjectActive()
+    {
+        Object.SetActive(true);
+    }
+}
