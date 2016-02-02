cd28ed87276236ec9a0a11ed3702675e2b62070c
bart-rijnders <mail@bartrijnders.com>
2016-02-02 14:23:11 +0100

    Added window and fields

---
+++ Assets/Scripts/ActionBar/UnitStackWindow/StackField.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StackField : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
+	private Image _image;
+	public StackField previous;
+
+	void Start() {
+		_image = GetComponent<Image> ();
+	}
+
+	public void Set(Sprite sprite){
+		_image.sprite = sprite;
+		_image.enabled = true;
+	}
+
+	public void Hide() {
+		_image.sprite = null;
+		_image.color = Color.white;
+		_image.enabled = false;
+	}
+
+	public void OnPointerClick(PointerEventData e){
+		
+	}
+
+	public void OnPointerEnter(PointerEventData e){
+		_image.color = Color.blue;
+		if (previous != null)
+			previous.OnPointerEnter (e);
+	}
+
+	public void OnPointerExit(PointerEventData e){
+		_image.color = Color.white;
+	}
+}
---
+++ Assets/Scripts/ActionBar/UnitStackWindow/StackWindow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StackWindow : MonoBehaviour {
+
+	public StackField[] StackFields;
+	public Sprite[] Sprites;
+	private Image _image;
+
+	void Start() {
+		_image = GetComponent<Image> ();
+	}
+
+	void Show(int amount, string unitName) {
+		Sprite sprite = new Sprite ();
+
+		switch (unitName) {
+		case "Soldier":
+			sprite = Sprites [0];
+			break;
+		case "Tank":
+			sprite = Sprites [1];
+			break;
+		case "Robot":
+			sprite = Sprites [2];
+			break;
+		}
+
+		_image.enabled = true;
+		for (int i = 0; i < amount; i++) {
+			StackFields [i].Set (sprite);
+		}
+	}
+
+	void Hide() {
+		_image.enabled = false;
+		foreach (StackField stackField in StackFields) {
+			stackField.Hide ();
+		}
+	}
+}
