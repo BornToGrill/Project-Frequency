39eb9018ea0ecb32bc9bab60e589766fac29e096
bart-rijnders <mail@bartrijnders.com>
2016-02-08 14:04:51 +0100

    Added credits screen

---
+++ Assets/Scripts/TextCrawl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextCrawl : MonoBehaviour {
+
+	public float speed;
+	private RectTransform _rectTransform;
+
+	// Use this for initialization
+	void Start () {
+		_rectTransform = transform as RectTransform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_rectTransform.anchoredPosition = new Vector2 (_rectTransform.anchoredPosition.x, _rectTransform.anchoredPosition.y + speed * Time.deltaTime);
+
+
+	}
+}
